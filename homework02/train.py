99d33520d9e9d433f572d5cd826f34bd2eb48cd2
Oleg Polivin <olegpolivin@gmail.com>
2020-03-19 22:31:05 +0100

    hw2: done

--- homework02/train.py
+++ homework02/train.py
@@ -9,6 +9,7 @@ import torchvision
 from torchvision import transforms
 from torch.utils.data import SubsetRandomSampler
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.checkpoint import checkpoint_sequential
 import time
 
 
@@ -22,14 +23,14 @@ class TestDataset(Dataset):
 
     def __len__(self):
         return len(self.labels_frame) - 1
-    
+
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
         img_name = os.path.join(self.root_folder,
                                 self.labels_frame.loc[idx, 'imname'])
         image = io.imread(img_name)
-        
+
         # Treating greyscale images
         if len(image.shape) < 3:
             image = skimage.color.grey2rgb(image, alpha=None)
@@ -40,55 +41,204 @@ class TestDataset(Dataset):
         return image, category
 
 
+class MyGreatModel(nn.Module):
+
+    def __init__(self):
+        super(MyGreatModel, self).__init__()
+
+        self.model = nn.Sequential()
+        self.model.add_module('conv1', nn.Conv2d(3, 200, kernel_size=(3, 3), stride=1))
+        self.model.add_module('bn1_1', nn.BatchNorm2d(200))
+        self.model.add_module('relu1_1', nn.ReLU())
+        self.model.add_module('conv1_2', nn.Conv2d(200, 200, kernel_size=(3, 3), stride=1))
+        self.model.add_module('bn1_2', nn.BatchNorm2d(200))
+        self.model.add_module('relu1_2', nn.ReLU())
+        self.model.add_module('maxpool1', nn.MaxPool2d(3))
+
+        self.model.add_module('conv2_1', nn.Conv2d(200, 400, kernel_size=(3, 3), stride=1))
+        self.model.add_module('bn2_1', nn.BatchNorm2d(400))
+        self.model.add_module('relu2_1', nn.ReLU())
+        self.model.add_module('conv2_2', nn.Conv2d(400, 400, kernel_size=(3, 3), stride=1))
+        self.model.add_module('bn2_2', nn.BatchNorm2d(400))
+        self.model.add_module('relu2_2', nn.ReLU())
+        self.model.add_module('maxpool2', nn.MaxPool2d(3))
+
+        self.model.add_module('flatten', nn.Flatten())
+        self.model.add_module('fc1', nn.Linear(10000, 1000))
+        self.model.add_module('dp1', nn.Dropout(0.5))
+        self.model.add_module('fc2', nn.Linear(1000, 200))
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+
+class CheckpointedModel(nn.Module):
+
+    def __init__(self, arrangement_num=0):
+        super(CheckpointedModel, self).__init__()
+        self.arrangement_num = arrangement_num
+
+        self.features_1 = nn.Sequential(
+            nn.Conv2d(3, 200, kernel_size=(3, 3), stride=1)
+        )
+        self.bn_1 = nn.BatchNorm2d(200)
+        self.features_2 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(200, 200, kernel_size=(3, 3), stride=1)
+        )
+        self.bn_2 = nn.BatchNorm2d(200)
+        self.features_3 = nn.Sequential(
+            nn.ReLU(),
+            nn.MaxPool2d(3),
+            nn.Conv2d(200, 400, kernel_size=(3, 3), stride=1)
+        )
+        self.bn_3 = nn.BatchNorm2d(400)
+        self.features_4 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(400, 400, kernel_size=(3, 3), stride=1)
+        )
+        self.bn_4 = nn.BatchNorm2d(400)
+        self.features_5 = nn.Sequential(
+            nn.ReLU(),
+            nn.MaxPool2d(3),
+            nn.Flatten(),
+            nn.Linear(10000, 1000)
+        )
+        self.dp1 = nn.Dropout(0.5)
+        self.classifier = nn.Linear(1000, 200)
+
+    def forward(self, x):
+        '''
+        Arrangement 0: as it is, only blocks, no checkpoiting
+        Arrangement 1: Added checkpoint_sequential(self.features_1, 1, x)
+        etc...
+        '''
+        arrangement = self.arrangement_num
+
+        # Arrangement 0
+        if arrangement == 0:
+            x = self.features_1(x)
+            x = self.bn_1(x)
+            x = self.features_2(x)
+            x = self.bn_2(x)
+            x = self.features_3(x)
+            x = self.bn_3(x)
+            x = self.features_4(x)
+            x = self.bn_4(x)
+            x = self.features_5(x)
+            x = self.dp1(x)
+            x = self.classifier(x)
+            return x
+
+        # Arrangement 1
+        if arrangement == 1:
+            x = checkpoint_sequential(self.features_1, 1, x)
+            x = self.bn_1(x)
+            x = self.features_2(x)
+            x = self.bn_2(x)
+            x = self.features_3(x)
+            x = self.bn_3(x)
+            x = self.features_4(x)
+            x = self.bn_4(x)
+            x = self.features_5(x)
+            x = self.dp1(x)
+            x = self.classifier(x)
+            return x
+
+        # Arrangement 2
+        if arrangement == 2:
+            x = checkpoint_sequential(self.features_1, 1, x)
+            x = self.bn_1(x)
+            x = checkpoint_sequential(self.features_2, 2, x)
+            x = self.bn_2(x)
+            x = self.features_3(x)
+            x = self.bn_3(x)
+            x = self.features_4(x)
+            x = self.bn_4(x)
+            x = self.features_5(x)
+            x = self.dp1(x)
+            x = self.classifier(x)
+            return x
+
+        # Arrangement 3
+        if arrangement == 3:
+            x = checkpoint_sequential(self.features_1, 1, x)
+            x = self.bn_1(x)
+            x = checkpoint_sequential(self.features_2, 2, x)
+            x = self.bn_2(x)
+            x = checkpoint_sequential(self.features_3, 3, x)
+            x = self.bn_3(x)
+            x = self.features_4(x)
+            x = self.bn_4(x)
+            x = self.features_5(x)
+            x = self.dp1(x)
+            x = self.classifier(x)
+            return x
+
+        # Arrangement 4
+        if arrangement == 4:
+            x = checkpoint_sequential(self.features_1, 1, x)
+            x = self.bn_1(x)
+            x = checkpoint_sequential(self.features_2, 2, x)
+            x = self.bn_2(x)
+            x = checkpoint_sequential(self.features_3, 3, x)
+            x = self.bn_3(x)
+            x = checkpoint_sequential(self.features_4, 2, x)
+            x = self.bn_4(x)
+            x = self.features_5(x)
+            x = self.dp1(x)
+            x = self.classifier(x)
+            return x
+
+        # Arrangement 5
+        if arrangement == 5:
+            x = checkpoint_sequential(self.features_1, 1, x)
+            x = self.bn_1(x)
+            x = checkpoint_sequential(self.features_2, 2, x)
+            x = self.bn_2(x)
+            x = checkpoint_sequential(self.features_3, 3, x)
+            x = self.bn_3(x)
+            x = checkpoint_sequential(self.features_4, 2, x)
+            x = self.bn_4(x)
+            x = checkpoint_sequential(self.features_5, 4, x)
+            x = self.dp1(x)
+            x = self.classifier(x)
+            return x
+
+
 def load_train_data(batch_size):
     """Function to load data, split to train/validation set.
     A predefined list of transforms is applied.
     """
-    
+
     transform_train = transforms.Compose([
-        transforms.RandomHorizontalFlip(),
+        transforms.RandomChoice(
+            [transforms.RandomRotation(5),
+            transforms.RandomHorizontalFlip(0.3),
+            transforms.RandomPerspective(distortion_scale=0.5, p=0.5),
+            transforms.RandomAffine(2, scale=(1, 1.3))]),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
     dataset = torchvision.datasets.ImageFolder('tiny-imagenet-200/train',
-                                                transform=transform_train)
+                                                        transform=transform_train)
     train_dataset, val_dataset = torch.utils.data.random_split(dataset, [80000, 20000])
 
     # Dictionary that maps classes to indices
     class_to_idx = dataset.class_to_idx
-    train_loader = torch.utils.data.DataLoader(train_dataset, 
+    train_loader = torch.utils.data.DataLoader(train_dataset,
                                 batch_size=batch_size,
                                 shuffle=True,
-                                num_workers=1)
+                                num_workers=4)
 
-    val_loader = torch.utils.data.DataLoader(val_dataset, 
+    val_loader = torch.utils.data.DataLoader(val_dataset,
                                 batch_size=batch_size,
                                 shuffle=True,
-                                num_workers=1)
+                                num_workers=4)
     return train_loader, val_loader, class_to_idx
 
-def define_model():
-    model = nn.Sequential()
-    model.add_module('conv1', nn.Conv2d(3, 64, kernel_size=(3,3), stride=1))
-    model.add_module('bn1', nn.BatchNorm2d(64))
-    model.add_module('relu1', nn.ReLU())
-    model.add_module('maxpool1', nn.MaxPool2d(2))
-
-    model.add_module('conv2', nn.Conv2d(64, 128, kernel_size=(3,3), stride=1))
-    model.add_module('bn2', nn.BatchNorm2d(128))
-    model.add_module('relu2', nn.ReLU())
-    model.add_module('maxpool2', nn.MaxPool2d(2))
-
-    model.add_module('flatten', nn.Flatten())
-    model.add_module('fc1', nn.Linear(25088, 512))
-    model.add_module('bn3', nn.BatchNorm1d(512))
-    model.add_module('lrelu1', nn.LeakyReLU())
-    model.add_module('dp1', nn.Dropout(0.7))
-
-    model.add_module('fc2', nn.Linear(512, 200))
-    model = model.to(device)
-    return model
 
 def train_model(
     model,
@@ -98,37 +248,43 @@ def train_model(
     num_epochs,
     device,
     loss_fn,
-    scheduler = None):
-    
+    batches_per_update,
+    scheduler=None):
+
     train_loss = []
     val_accuracy = []
     ave_time_per_batch = 0
     model.train(True)
+
     for epoch in range(num_epochs):
         start_time = time.time()
         start_epoch_counter = time.perf_counter()
-        for x_batch, y_batch in train_loader:
+
+        # 1 Clear the gradients
+        optimizer.zero_grad()
+
+        for batch_i, (x_batch, y_batch) in enumerate(train_loader):
             x_batch = x_batch.to(device)
             y_batch = y_batch.to(device)
-            # I. Training                                        
-            #2 Clear the gradients
-            optimizer.zero_grad()
-            
-            #3 Forward
+            # I. Training
+
+            # 2 Forward
             start_counter = time.perf_counter()
             predictions = model.forward(x_batch)
             elapsed_time = time.perf_counter() - start_counter
             ave_time_per_batch += elapsed_time/batch_size
 
-            #4 Calculating loss
+            # 3 Calculating loss
             loss = loss_fn(predictions, y_batch)
-            
-            #5 Calculating gradients
+
+            # 4 Calculating gradients
             loss.backward()
-            
-            #6 Optimizer step
-            optimizer.step()
-            
+
+            # 5 Optimizer step - Adapted for accumulation of gradients
+            if (batch_i + 1) % batches_per_update == 0:
+                optimizer.step()
+                optimizer.zero_grad()
+
             # II. Tracking the training
             train_loss.append(loss.cpu().data.numpy())
         if scheduler:
@@ -137,13 +293,15 @@ def train_model(
         elapsed_epoch_time = time.perf_counter() - start_epoch_counter
 
         # III. Validation
-        model.train(False) # disable dropout / use averages for batch_norm
+        model.train(False)  # disable dropout / use averages for batch_norm
         for x_batch, y_batch in val_loader:
             x_batch = x_batch.to(device)
             y_batch = y_batch.to(device)
             predictions = model.forward(x_batch)
             y_pred = predictions.max(1)[1].data
             val_accuracy.append(np.mean( (y_batch.cpu() == y_pred.cpu()).numpy() ))
+
+        validation_accuracy = np.mean(val_accuracy[-20000 // batch_size:]) * 100
         # IV. Reporting
         # Then we print the results for this epoch:
         print("Epoch {} of {} took {:.3f}s".format(
@@ -152,10 +310,13 @@ def train_model(
         print("  (added) on average one batch of size {}s is processed in {:.3f}s".format(
             batch_size, ave_time_per_batch))
         print("  training loss (in-iteration): \t{:.6f}".format(
-            np.mean(train_loss[-80000 // batch_size :])))
+            np.mean(train_loss[-80000 // batch_size:])))
         print("  validation accuracy: \t\t\t{:.2f} %".format(
-            np.mean(val_accuracy[-20000 // batch_size :]) * 100))
-    
+            validation_accuracy))
+
+        if validation_accuracy > 40:
+            print(f'Fitted the model to exceed 40% on the validation set. Exiting loop on epoch {epoch + 1}.')
+            break
     return model, train_loss, val_accuracy
 
 
@@ -164,20 +325,20 @@ def evaluate_on_test_data(batch_size, class_to_idx):
     print('Evaluating on test data')
     labels = pd.read_csv('tiny-imagenet-200/val/val_annotations.txt', sep='\t', header=None)
     labels.columns = ['imname', 'id', 'bb1', 'bb2', 'bb3', 'bb4']
-    
+
     test_dataset = TestDataset(root_folder='tiny-imagenet-200/val/images/',
                            labels_frame=labels,
                            class_to_idx=class_to_idx,
-                           transform = transforms.Compose([
+                           transform=transforms.Compose([
                                transforms.ToTensor(),
                                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
                            ]))
-    test_loader = torch.utils.data.DataLoader(test_dataset, 
+    test_loader = torch.utils.data.DataLoader(test_dataset,
                                 batch_size=batch_size,
                                 shuffle=True,
-                                num_workers=1)
+                                num_workers=4)
 
-    model.eval()
+    model.train(False)
     correct_samples = 0
     total_samples = 0
     for x_batch, y_batch in test_loader:
@@ -209,13 +370,15 @@ if __name__ == '__main__':
     parser.add_argument('--bs', type=int, default=64, help='batch size that will be used for training')
     parser.add_argument('--lr', type=float, default=0.0001, help='learning rate')
     parser.add_argument('--num_epochs', type=int, default=100, help='num epochs to train')
-    parser.add_argument('--wd', type=float, default=0.001, help='regularization coef')
-    parser.add_argument('--gamma', type=float, default=0.9, help='decrease lr by this coef every 8 epochs')
+    parser.add_argument('--wd', type=float, default=0.00001, help='regularization coef')
+    parser.add_argument('--gamma', type=float, default=0.1, help='decrease lr by this coef every 8 epochs')
     parser.add_argument('--evaluate', action='store_true', help='evaluate on the test set')
     parser.add_argument('--sched', action='store_true', help='use scheduler or not')
-    
+    parser.add_argument('--arrangement_num', type=int, default=None, help='use checkpointing arrangement')
+    parser.add_argument('--ebs', type=int, default=None, help='effective batch size for gradient accumulation')
+
     args = parser.parse_args()
-    
+
     batch_size = args.bs
     lr = args.lr
     num_epochs = args.num_epochs
@@ -223,18 +386,29 @@ if __name__ == '__main__':
     gamma = args.gamma
     evaluate = args.evaluate
     use_scheduler = args.sched
+    arrangement_num = args.arrangement_num
+    effective_batch_size = args.ebs
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     train_loader, val_loader, class_to_idx = load_train_data(batch_size)
-    model = define_model()
 
-    loss_fn = nn.CrossEntropyLoss()    
-    #L2 regularization is added through weight_decay
+    if arrangement_num:
+        model = CheckpointedModel(arrangement_num)
+    else:
+        model = MyGreatModel()
+    model = model.to(device)
+    loss_fn = nn.CrossEntropyLoss()
+    # L2 regularization is added through weight_decay
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
     scheduler = None
     if use_scheduler:
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=8, gamma=gamma)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=15, gamma=gamma)
+
+    if effective_batch_size:
+        batches_per_update = effective_batch_size / batch_size
+    else:
+        batches_per_update = 1  # That is update every batch
 
     model, train_loss, val_accuracy = train_model(
                                                 model,
@@ -244,9 +418,10 @@ if __name__ == '__main__':
                                                 num_epochs,
                                                 device,
                                                 loss_fn,
-                                                scheduler = scheduler)
+                                                batches_per_update,
+                                                scheduler=scheduler)
     if evaluate:
         evaluate_on_test_data(batch_size, class_to_idx)
-    
-    # Evaluate top 
+
+    # Evaluate memory usage
     print(f"Peak memory usage by Pytorch tensors: {(torch.cuda.max_memory_allocated() / 1024 / 1024):.2f} Mb")
